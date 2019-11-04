354c823746774238a080a529f8d73de4b2860e82
Sophie <l012265h@student.staffs.ac.uk>
2019-11-04 17:36:37 +0000

    tried to create an image
    need to either resize image or render to screen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "SDL.h"
 #include "SDL_image.h"
+#define _CSurface_H_
 
 using namespace std;
 
@@ -15,13 +16,23 @@ int main(int argc, char *argv[])
 	SDL_RenderPresent(renderer);
 	//SDL_Delay(300);
 
-	if (!IMG_Init(IMG_INIT_PNG) && IMG_INIT_PNG)
+	if (!IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)
 	{
-		cout << "Failed to initialise PNG image" << endl;
+		cout << "Failed to initialise PNG image: " << IMG_GetError << endl;
 	}
 
+	SDL_Surface* imageSurface = NULL;
+	SDL_Surface* windowSurface = NULL;
 
+	imageSurface = IMG_Load("Assets/Grass.png"); //Have to be in FarmingGame folder
 
+	if (imageSurface == NULL)
+	{
+		cout << "Failed to load image surface: " << IMG_GetError << endl;
+	}
+	imageSurface->w * 2222, imageSurface->h * 2222;
+	SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
+	SDL_UpdateWindowSurface(window);
 
 	cin.get();
 	return 0;
